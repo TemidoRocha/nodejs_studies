bd115571df800456cc0d03d9411838d11b2e7113
TemidoRocha <manueltemidorocha@gmail.com>
2021-07-02 07:31:01 +0100

    factory function and builder pattern

--- Ex_Files_Node_js_Design_Patterns/Ch02/builderPattern/index.js
+++ Ex_Files_Node_js_Design_Patterns/Ch02/builderPattern/index.js
@@ -1,17 +1,21 @@
 var PersonBuilder = require('./PersonBuilder');
 
+/**
+ * Builder Pattern
+ *
+ * "separate the construction of a complex object from its representation
+ *  so than the same contruction process can create different representations."
+ */
+
 // Employees
 var sue = new PersonBuilder('Sue').makeEmployee().makeManager(60).build();
 var bill = new PersonBuilder('Bill').makeEmployee().makePartTime().build();
 var phil = new PersonBuilder('Phil').makeEmployee().build();
 
 // Shoppers
-var charles = new PersonBuilder('Charles')
-    .withMoney(500)
-    .withList(['jeans', 'sunglasses'])
-    .build();
+var charles = new PersonBuilder('Charles').withMoney(500).withList(['jeans', 'sunglasses']).build();
 
 var tabbitha = new PersonBuilder('Tabbitha').withMoney(1000).build();
 
-console.log( sue.toString() );
-console.log( charles.toString() );
+console.log(sue.toString());
+console.log(charles.toString());
--- Ex_Files_Node_js_Design_Patterns/Ch02/factoryFunction/index.js
+++ Ex_Files_Node_js_Design_Patterns/Ch02/factoryFunction/index.js
@@ -3,7 +3,7 @@ var userFactory = require('./userFactory');
 var alex = userFactory('Alex Banks', 100);
 var eve = userFactory('Eve Porcello', 100, 'employee', 'This and That');
 
-eve.payDay(100);
+eve.payDay(299);
 
-console.log( alex.toString() );
-console.log( eve.toString() );
+console.log(alex.toString());
+console.log(eve.toString());
